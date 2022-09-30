e9cd16fb144320eaae6821485bd90c6d384876c1
Huzaifa yusuf <huothyfayusuf1993@gmail.com>
2022-09-30 23:11:52 +0100

    add all style and js

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+    var sin = document.getElementById("myLinks");
+    if (sin.style.display === "block") {
+      sin.style.display = "none";
+    } else {
+      sin.style.display = "block";
+    }
+  }
